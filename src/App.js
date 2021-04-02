bbaceea41cac0427e267dea26b7948884112f76a
Alazar-dev <mealazartadesse@gmail.com>
2021-04-02 12:10:36 +0300

    imported theme and provided for App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from './themes/Theme'
 
 import Header from "./layouts/header/AppBar";
 import Home from "./pages/Home";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Router>
-          <Header />
-          <Switch>
-            <Route path="/" component={Home} />
-          </Switch>
-          <Footer />
+          <ThemeProvider theme={theme}>
+            <Header />
+            <Switch>
+              <Route path="/" component={Home} />
+            </Switch>
+            <Footer />
+          </ThemeProvider>
         </Router>
       </div>
     );
