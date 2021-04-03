6efa1deab0379eb27b928c2d6fa5db1798935945
Alazar-dev <mealazartadesse@gmail.com>
2021-04-03 12:50:23 +0300

    gave spacing

--- src/pages/sermons/Sermons.js
+++ src/pages/sermons/Sermons.js
@@ -5,7 +5,6 @@ import {
   InputBase,
   List,
   ListItem,
-  Card,
   CardContent,
   Button,
   Container,
@@ -18,16 +17,17 @@ import GalleryCheckout from "../../components/gallery/GalleryCheckout";
 import ImgSupermacy from "../../assets/Sermons/placeholder1.png";
 import ImgTeach from "../../assets/Sermons/placeholder2.png";
 import ImgGospel from "../../assets/Sermons/placeholder3.png";
+import ImgChurch from "../../assets/Sermons/placeholder4.png";
 
 import headerImg from "../../assets/Sermons/Sermon.jpg";
 const useStyles = makeStyles((theme) => ({
   headerImg: {
     width: "100%",
-    marginTop: 30,
+    marginTop: 55,
   },
   sideContent: {
-      marginTop: 30
-  }
+    marginTop: 30,
+  },
 }));
 
 const SermonsPage = () => {
@@ -40,10 +40,10 @@ const SermonsPage = () => {
         </Typography>
       </Box>
       <Grid justify="center" container spacing={4}>
-        <Grid item sm={6} md={9} lg={6}>
+        <Grid container sm={6} md={9} lg={6} spacing={2}>
           <img className={classes.headerImg} src={headerImg} alt="" />
           <Grid item>
-            <Card>
+            <Box display="flex">
               <img src={ImgSupermacy} alt="" />
               <div>
                 <CardContent>
@@ -53,10 +53,10 @@ const SermonsPage = () => {
                   </Typography>
                 </CardContent>
               </div>
-            </Card>
+            </Box>
           </Grid>
           <Grid item>
-            <Card>
+            <Box display="flex">
               <img src={ImgTeach} alt="" />
               <div>
                 <CardContent>
@@ -69,10 +69,10 @@ const SermonsPage = () => {
                   </Typography>
                 </CardContent>
               </div>
-            </Card>
+            </Box>
           </Grid>
           <Grid item>
-            <Card>
+            <Box display="flex">
               <img src={ImgGospel} alt="" />
               <div>
                 <CardContent>
@@ -85,7 +85,23 @@ const SermonsPage = () => {
                   </Typography>
                 </CardContent>
               </div>
-            </Card>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Box display="flex">
+              <img src={ImgChurch} alt="" />
+              <div>
+                <CardContent>
+                  <Typography variant="subtitle2">
+                    THE GOSPEL SOUL & LIFE IN THE SERMON ON THE MOUNT FLOWER
+                    MOUND
+                  </Typography>
+                  <Typography variant="subtitle2" color="textSecondary">
+                    Category: Global Outreach peaker: Pastor Abebe Kebede
+                  </Typography>
+                </CardContent>
+              </div>
+            </Box>
           </Grid>
         </Grid>
         <Grid className={classes.sideContent} item sm={12} md={3} lg={3}>
