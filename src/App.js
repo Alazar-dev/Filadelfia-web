04e1cf258d5aacc242bb9acb91e18d5f6e8686cf
Alazar-dev <mealazartadesse@gmail.com>
2021-04-03 09:26:28 +0300

    added all routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./themes/Theme";
 
-import Header from "./layouts/header/AppBar";
 import Home from "./pages/Home";
+import Sermons from "./pages/sermons/Sermons";
+import PrayerWall from "./pages/prayerwall/PrayerWall";
+import Events from "./pages/events/Events";
+import Gallery from "./pages/gallery/Gallery";
+import Contact from "./pages/contact/Contact";
+
+import Header from "./layouts/header/AppBar";
 import Footer from "./layouts/footer/Footer";
 
 class App extends Component {
@@ -15,7 +21,12 @@ class App extends Component {
           <ThemeProvider theme={theme}>
             <Header />
             <Switch>
-              <Route path="/" component={Home} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/sermons" component={Sermons} />
+              <Route exact path="/prayerwall" component={PrayerWall} />
+              <Route exact path="/events" component={Events} />
+              <Route exact path="/gallery" component={Gallery} />
+              <Route exact path="/contact" component={Contact} />
             </Switch>
             <Footer />
           </ThemeProvider>
