2186900f808b6b1a2d67c4a89482aba7335b7518
ephremdeme <demelashephrem@gmail.com>
2021-04-04 14:40:43 +0300

    📦 NEW: adds sermon events section to home

---
+++ src/components/home/latestSermons/SermonEvents.jsx
@@ -0,0 +1,21 @@
+import { Box, Container, Grid } from "@material-ui/core";
+import React from "react";
+import EventsCard from "./Events";
+import LatestSermonsCards from "./LatestSermon";
+
+function SermonEvents() {
+  return (
+    <Container>
+      <Grid container spacing="4">
+        <Grid item xs={12} md={7}>
+          <LatestSermonsCards />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <EventsCard />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default SermonEvents;
