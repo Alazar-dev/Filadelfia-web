470a698f9bd9e71652ef81a6aff31486a79ac91d
Alazar-dev <mealazartadesse@gmail.com>
2021-04-06 15:06:50 +0300

    created route for eventsDetail

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const Sermons = lazy(() => import("./pages/sermons/Sermons"));
 const SermonsDetail = lazy(() => import("./pages/sermons/SermonsDetail"))
 const PrayerWall = lazy(() => import("./pages/prayerwall/PrayerWall"));
 const Events = lazy(() => import("./pages/events/Events"));
+const EventsDetail = lazy(() => import("./pages/events/EventsDetail"));
 const Gallery = lazy(() => import("./pages/gallery/Gallery"));
 const Contact = lazy(() => import("./pages/contact/Contact"));
 
@@ -30,6 +31,7 @@ class App extends Component {
                 <Route exact path="/sermonsdetail" component={SermonsDetail} />
                 <Route exact path="/prayerwall" component={PrayerWall} />
                 <Route exact path="/events" component={Events} />
+                <Route exact path="/eventsdetail" component={EventsDetail} />
                 <Route exact path="/gallery" component={Gallery} />
                 <Route exact path="/contact" component={Contact} />
               </Switch>
