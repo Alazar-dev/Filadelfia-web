95a69e1b4448a61eb611504d33d5b7ea9e164eb9
Alazar-dev <mealazartadesse@gmail.com>
2021-04-06 14:35:28 +0300

    created podcast component and main content

--- src/components/sermonsDetail/SermonsDetailLeft.jsx
+++ src/components/sermonsDetail/SermonsDetailLeft.jsx
@@ -7,6 +7,8 @@ import Typography from "@material-ui/core/Typography";
 import { Box, Button, Grid } from "@material-ui/core";
 import Event from "../../assets/Events/Event.jpg";
 import { AccessTime, LocationOn } from "@material-ui/icons";
+import Img from "../../assets/Sermon Details/img.png";
+import SermonAudio from "./SermonAudio";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -77,18 +79,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function EventLeft() {
+function SermonsDetailLeft() {
   const classes = useStyles();
   return (
     <Grid item md={8}>
       <Box py={5}>
         <Box py={2}>
-          <img className={classes.image} src={Event} alt="events coming" />
+          <SermonAudio />
         </Box>
         <Box>
           <Grid container justify="center" spacing={4}>
-            <EventItem />
-            <EventItem />
             <EventItem />
             <EventItem />
           </Grid>
@@ -114,32 +114,46 @@ export const EventItem = () => {
       <Card>
         <CardContent>
           <Grid container>
-            <Grid item xs={1} md={1}>
-              <Box className={classes.dateConf} bgcolor="secondary.dark">
-                <Typography className={classes.eventDate} color="primary">
-                  23
-                </Typography>
-                <Typography className={classes.eventMonth} color="primary">
-                  JAN
-                </Typography>
-              </Box>
-            </Grid>
             <Grid item xs={10} md={10}>
-              <Typography className={classes.eventTitle} variant="body1">
-                Phillipisiyos Church Women Yearly Conference
-              </Typography>
-              <Typography className={classes.date} variant="subtitle2">
-                <AccessTime className={classes.icons} /> Sunday
-                <span className={classes.yellowText}>09:45 am</span> to{" "}
-                <span className={classes.yellowText}>01:30pm</span>
-              </Typography>
-              <Typography className={classes.date} variant="subtitle2">
-                <LocationOn className={classes.icons} /> Bole Michael, infront
-                of Miachel Church
-              </Typography>
-            </Grid>
-            <Grid className={classes.itemButton} item md={1}>
-              <Button className={classes.button}>More</Button>
+              <Box className={classes.sermonBox}>
+                <img src={Img} alt="" />
+                <div>
+                  <CardContent>
+                    <Typography variant="body2">
+                      it is with the supremacy of Christ in your life. All the
+                      planets of your life — your sexuality and desires, your
+                      commitments and beliefs, your aspirations and dreams, your
+                      attitudes and convictions, your habits and disciplines,
+                      your solitude and relationships, your labor and leisure,
+                      your thinking and feeling — all the planets of your life
+                      are held in orbit by the greatness and gravity and blazing
+                      brightness of the supremacy of Jesus Christ at the center
+                      of your life. And if he ceases to be the bright, blazing,
+                      satisfying beauty at the center of your life, the planets
+                      will fly into confusion, and a hundred things will be out
+                      of control, and sooner or later they will crash into
+                      destruction.
+                    </Typography>
+                    <Box p={5}>
+                      <Typography color="primary" align="center">
+                        “If Jesus ceases to be the center of your life, all will
+                        crash into destruction.”
+                      </Typography>
+                    </Box>
+                    <Typography variant="body2">
+                      We were made to see and savor with everlasting
+                      satisfaction the supremacy of Christ. Our sexuality points
+                      to this and our sexuality is purified by this. We are
+                      sexual beings that we may know something more of the
+                      supremacy of Christ. And we must know the supremacy of
+                      Christ — we must know him in his supremacy — in order to
+                      experience our sexuality as sacred and sweet and
+                      Christ-exalting — and secondary, quietly, powerfully
+                      secondary.
+                    </Typography>
+                  </CardContent>
+                </div>
+              </Box>
             </Grid>
           </Grid>
         </CardContent>
@@ -148,4 +162,4 @@ export const EventItem = () => {
   );
 };
 
-export default EventLeft;
+export default SermonsDetailLeft;
