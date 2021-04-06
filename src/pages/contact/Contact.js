819aa9a0360242a24b294f6152688344c4222b90
ephremdeme <demelashephrem@gmail.com>
2021-04-06 18:52:14 +0300

    👌 IMPROVE: resposiveness of map image

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,11 +1,27 @@
-import { Box, Container, Grid } from "@material-ui/core";
+import {
+  Box,
+  Container,
+  createStyles,
+  Grid,
+  makeStyles,
+} from "@material-ui/core";
 import BreadcumbHero from "../../components/breadcrumbs/BreadcrumbsHero";
 import ContactAddress from "../../components/contact/ContactAddress";
 import ContactForm from "../../components/contact/ContactForm";
 import BottomGallery from "../../components/home/BottomGallery";
 import ContactMap from "../../assets/Contact/Map.jpg";
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    map: {
+      maxWidth: "100%",
+      maxHeight: "100%",
+    },
+  })
+);
+
 const Contact = () => {
+  const classes = useStyles();
   return (
     <div>
       <BreadcumbHero title="Contact Us" />
@@ -18,7 +34,7 @@ const Contact = () => {
         </Container>
       </Box>
       <Box>
-        <img src={ContactMap} alt="Map" />
+        <img className={classes.map} src={ContactMap} alt="Map" />
       </Box>
       <BottomGallery />
     </div>
